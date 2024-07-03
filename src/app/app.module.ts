76765a8b9e525953b57fb181de7cc890cc46c1d3
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-07-03 20:10:03 +0530

    chore: Update Angular CLI and dependencies to version 18.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { counterReducer } from './store/counter.reducer';
-import { StoreModule } from '@ngrx/store';
 import { CounterEffects } from './store/counter.effects';
-import { EffectsModule } from '@ngrx/effects';
-import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { counterReducer } from './store/counter.reducer';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterOutputComponent,
-    CounterControlsComponent
+    CounterControlsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({
-      counter: counterReducer
+      counter: counterReducer,
     }),
     EffectsModule.forRoot([CounterEffects]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
